b2636a66ec1a0ef3f4e6230bbb552d6bd1f8eae1
lexggwp <alexeyfilipp@gmail.com>
2022-11-29 16:17:51 +0300

    added: hm8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,14 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType):  UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
+        case 'sort': // by name
+            let newArr = [...state]
+                newArr.sort( (a, b) => a.name > b.name ? 1: -1)
+            return action.payload === 'up'? newArr: newArr.reverse();
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
